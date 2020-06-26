6d307c97a3fbb59bb4c1918c0970cfc13e6e4d55
rcacomps <63969658+rcacomps@users.noreply.github.com>
2020-06-25 23:06:21 -0500

    condensed js

--- js/sq.js
+++ js/sq.js
@@ -1,100 +1,80 @@
-var seqlen=40;
-var numcub=5;
-var seq=new Array();
-var posit = new Array();
-function scramble(){
-	var i,j,ls,n,f;
-	for(n=0; n<numcub; n++){
-		posit = new Array(0,0,1,2,2,3,4,4,5,6,6,7,8,9,9,10,11,11,12,13,13,14,15,15);
-		ls=-1;
-		seq[n]=new Array();
-		f=0;
-		for(i=0; i<seqlen; i++){
-			do{
-				if(ls==0){
-					j=Math.floor(Math.random()*22)-11;if(j>=0) j++;
-				}
-				else if(ls==1){
-					j=Math.floor(Math.random()*12)-11;
-				}
-				else if(ls==2){
-					j=0;
-				}
-				else{
-					j=Math.floor(Math.random()*23)-11;
-				}
-			}
-			while((f>1 && j>=-6 && j<0) || domove(j) );
-			if(j>0) ls=1;
-			else if(j<0) ls=2;
-			else {
-				ls=0; f++;
-			}
-			seq[n][i]=j;
-		}
-	}
+var seqlen = 40
+  , numcub = 5
+  , seq = new Array
+  , posit = new Array;
+function d() {
+    for (var o, r, t, e, i = 0; i < numcub; i++)
+        for (posit = new Array(0,0,1,2,2,3,4,4,5,6,6,7,8,9,9,10,11,11,12,13,13,14,15,15),
+        t = -1,
+        seq[i] = new Array,
+        o = e = 0; o < seqlen; o++) {
+            for (; 0 == t ? 0 <= (r = Math.floor(22 * Math.random()) - 11) && r++ : r = 1 == t ? Math.floor(12 * Math.random()) - 11 : 2 == t ? 0 : Math.floor(23 * Math.random()) - 11,
+            1 < e && -6 <= r && r < 0 || domove(r); )
+                ;
+            0 < r ? t = 1 : r < 0 ? t = 2 : (t = 0,
+            e++),
+            seq[i][o] = r
+        }
 }
-function scramblestring(n){
-	var s="",i,k,l=-1;
-	for(i=0; i<seq[n].length; i++){
-		k=seq[n][i];
-		if(k==0){
-			if(l==-1) s+="(0,0)  ";
-			if(l==1) s+="0)  ";
-			if(l==2) s+=")  ";
-			l=0;
-		}
-		else if(k>0){
-			s+= "(" + (k>6?k-12:k)+",";l=1;
-		}
-		else if(k<0){
-			if(l<=0) s+="(0,";
-			s+=(k<=-6?k+12:k);
-			l=2;
-		}
-	}
-	if(l==1) s+="0";
-	if(l!=0) s+=")";
-	return s;
+function q(o) {
+    for (var r, t = "", e = -1, i = 0; i < seq[o].length; i++)
+        0 == (r = seq[o][i]) ? (-1 == e && (t += "(0,0)  "),
+        1 == e && (t += "0)  "),
+        2 == e && (t += ")  "),
+        e = 0) : 0 < r ? (t += "(" + (6 < r ? r - 12 : r) + ",",
+        e = 1) : r < 0 && (e <= 0 && (t += "(0,"),
+        t += r <= -6 ? r + 12 : r,
+        e = 2);
+    return 1 == e && (t += "0"),
+    0 != e && (t += ")"),
+    t
 }
-function domove(m){
-	var i,c,t,f=m;
-	if(f==0){
-		for(i=0; i<6; i++){
-			c=posit[i+12];
-			posit[i+12]=posit[i+6];
-			posit[i+6]=c;
-		}
-	}
-	else if(f>0){
-		f=12-f;
-		if(posit[f]==posit[f-1]) return true;
-		if(f<6 && posit[f+6]==posit[f+5]) return true;
-		if(f>6 && posit[f-6]==posit[f-7]) return true;
-		if(f==6 && posit[0]==posit[11]) return true;
-		t=new Array();
-		for(i=0;i<12;i++) t[i]=posit[i];
-		c=f;
-		for(i=0;i<12;i++){
-			posit[i]=t[c];
-			if(c==11)c=0;
-			else c++;
-		}
-	}
-	else if(f<0){
-		f=-f;
-		if(posit[f+12]==posit[f+11]) return true;
-		if(f<6 && posit[f+18]==posit[f+17]) return true;
-		if(f>6 && posit[f+6]==posit[f+5]) return true;
-		if(f==6 && posit[12]==posit[23]) return true;
-		t=new Array();
-		for(i=0;i<12;i++) t[i]=posit[i+12];
-		c=f;
-		for(i=0;i<12;i++){
-			posit[i+12]=t[c];
-			if(c==11)c=0;
-			else c++;
-		}
-	}
-	return false;
+function domove(o) {
+    var r, t, e, i = o;
+    if (0 == i)
+        for (r = 0; r < 6; r++)
+            t = posit[r + 12],
+            posit[r + 12] = posit[r + 6],
+            posit[r + 6] = t;
+    else if (0 < i) {
+        if (posit[i = 12 - i] == posit[i - 1])
+            return !0;
+        if (i < 6 && posit[i + 6] == posit[i + 5])
+            return !0;
+        if (6 < i && posit[i - 6] == posit[i - 7])
+            return !0;
+        if (6 == i && posit[0] == posit[11])
+            return !0;
+        for (e = new Array,
+        r = 0; r < 12; r++)
+            e[r] = posit[r];
+        for (t = i,
+        r = 0; r < 12; r++)
+            posit[r] = e[t],
+            11 == t ? t = 0 : t++
+    } else if (i < 0) {
+        if (posit[(i = -i) + 12] == posit[i + 11])
+            return !0;
+        if (i < 6 && posit[i + 18] == posit[i + 17])
+            return !0;
+        if (6 < i && posit[i + 6] == posit[i + 5])
+            return !0;
+        if (6 == i && posit[12] == posit[23])
+            return !0;
+        for (e = new Array,
+        r = 0; r < 12; r++)
+            e[r] = posit[r + 12];
+        for (t = i,
+        r = 0; r < 12; r++)
+            posit[r + 12] = e[t],
+            11 == t ? t = 0 : t++
+    }
+    return !1
+}
+d();
+for (var i = 0; i < numcub; i++) {
+    var e = document.createElement("p")
+      , g = document.createTextNode(i + 1 + ". " + q(i));
+    e.appendChild(g),
+    document.getElementById("s").appendChild(e)
 }
--- js/view.js
+++
@@ -1,7 +0,0 @@
-scramble();
-for(var i = 0;i < numcub;i++){
-	var node = document.createElement("p");
-	var textnode = document.createTextNode((i + 1 + ". ") + scramblestring(i));
-	node.appendChild(textnode);
-	document.getElementById("scram").appendChild(node);
-}
